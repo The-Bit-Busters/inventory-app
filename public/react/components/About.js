f4ae92e8340e26bee21ffca7c138a0e5c9a01b89
Sonam Tsering <sonam.tsering@sonam.tsering-MacBook-Pro>
2023-03-22 11:07:42 -0400

    styling about page

--- public/react/components/About.js
+++ public/react/components/About.js
@@ -4,10 +4,10 @@ import '../stylesheets/about.css'
 const About = () => {
   return (
     <div className="wrapper">
-        <div className="box a">Mission Statement:  Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui, ut sed? Aperiam nisi repudiandae quas aspernatur minus labore quia quae facilis, doloribus, recusandae, at velit similique explicabo amet nostrum veritatis!</div>
-        <div className="box b">Core Values: Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui, ut sed? Aperiam nisi repudiandae quas aspernatur minus labore quia quae facilis, doloribus, recusandae, at velit similique explicabo amet nostrum veritatis!</div>
-        <div className="box c">Company History: Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui, ut sed? Aperiam nisi repudiandae quas aspernatur minus labore </div>
-        <div className="box d">Target Market: Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui, ut sed? Aperiam nisi repudiandae quas aspernatur minus labore quia quae facilis, doloribus, recusandae, at velit similique explicabo amet nostrum veritatis!</div>
+        <div className="box a"> <span>Mission Statement</span>:  Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui, ut sed? Aperiam nisi repudiandae quas aspernatur minus labore quia quae facilis, doloribus, recusandae, at velit similique explicabo amet nostrum veritatis!</div>
+        <div className="box b"><span>Core Values</span>: Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui, ut sed? Aperiam nisi repudiandae quas aspernatur minus labore quia quae facilis, doloribus, recusandae, at velit similique explicabo amet nostrum veritatis!</div>
+        <div className="box c"><span>Company History</span>: Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui, ut sed? Aperiam nisi repudiandae quas aspernatur minus labore </div>
+        <div className="box d"><span>Target Market</span>: Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui, ut sed? Aperiam nisi repudiandae quas aspernatur minus labore quia quae facilis, doloribus, recusandae, at velit similique explicabo amet nostrum veritatis!</div>
         <div className="box e">Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui, ut sed? Aperiam nisi repudiandae quas aspernatur minus labore quia quae facilis, doloribus, recusandae, at velit similique explicabo amet nostrum veritatis!</div>
     </div>
   )
