502d4e4444b1254de2d3394244063dd14e199c47
Nathan Cai <nathan97cai@gmail.com>
2023-03-22 13:44:14 -0400

    fixed up styling on some files and added small changes to footer and nav

--- public/react/components/App.js
+++ public/react/components/App.js
@@ -1,48 +1,45 @@
-import React, { useState, useEffect } from 'react';
-import NavBar  from './NavBar';
-import { HomePage } from './HomePage';
-import ShopAll from './ShopAll';
-import { Route, Routes, useLocation } from 'react-router-dom';
-import '../stylesheets/app.css';
-import About from './About';
-// import and prepend the api url to any fetch calls
-import apiURL from '../api';
-import Contact from './Contact';
-import Footer from './Footer';
-import Cart from './Cart';
+import React, { useState, useEffect } from "react";
+import NavBar from "./NavBar";
+import { HomePage } from "./HomePage";
+import ShopAll from "./ShopAll";
+import { Route, Routes, useLocation } from "react-router-dom";
+import "../stylesheets/app.css";
+import About from "./About";
+import apiURL from "../api";
+import Contact from "./Contact";
+import Footer from "./Footer";
+import Cart from "./Cart";
 
 export const App = () => {
+  const [items, setItems] = useState([]);
 
-	const [items, setItems] = useState([]);
+  async function fetchItems() {
+    try {
+      const response = await fetch(`${apiURL}/items`);
+      const itemsData = await response.json();
+      setItems(itemsData);
+    } catch (err) {
+      console.log("Oh no an error! ", err);
+    }
+  }
 
-	async function fetchItems(){
-		try {
-			const response = await fetch(`${apiURL}/items`);
-			const itemsData = await response.json();
-			// console.log(itemsData);
-			setItems(itemsData);
-		} catch (err) {
-			console.log("Oh no an error! ", err)
-		}
-	}
+  useEffect(() => {
+    fetchItems();
+  }, []);
 
-	useEffect(() => {
-		fetchItems();
-	}, []);
-
-	return (
-		<main className='appContainer'>	
-			<NavBar />
-			<div className='mainContainer'>
-			<Routes>
-				<Route path="/" element={<HomePage items={items}/>} />
-				<Route path="/shopall" element={<ShopAll />} />
-				<Route path="/about" element={<About />} />
-				<Route path="/contact" element={<Contact />} />
-				<Route path="/cart" element={<Cart />} />
-			</Routes>
-			</div>
-			<Footer />
-		</main>
-	)
-}
+  return (
+    <main className="appContainer">
+      <NavBar />
+      <div className="mainContainer">
+        <Routes>
+          <Route path="/" element={<HomePage items={items} />} />
+          <Route path="/shopall" element={<ShopAll />} items={items} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/cart" element={<Cart />} items={items} />
+        </Routes>
+      </div>
+      <Footer />
+    </main>
+  );
+};
--- public/react/components/NavBar.js
+++ public/react/components/NavBar.js
@@ -1,40 +1,45 @@
-import React from 'react'
-import '../stylesheets/navbar.css'
-import { Link } from 'react-router-dom';
-import { IoIosAddCircleOutline, IoIosSearch } from 'react-icons/io';
+import React from "react";
+import "../stylesheets/navbar.css";
+import { Link } from "react-router-dom";
+import { IoIosAddCircleOutline, IoIosSearch } from "react-icons/io";
 
 const NavBar = () => {
   return (
-    <div  style={{
-        backgroundColor: 'white',
-        borderBottom: '.5px solid rgb(172, 172, 172)',
-      }}>
-        <nav className='nav-bar navbar-static-top'>
-                <Link to='/' style={{ textDecoration: 'none' }}>
-                    <h1>Bit Buckets</h1>
-                </Link>
-            <form>
-                <IoIosSearch className='searchIcon' size='20px' />
-                <input className='searchBar' type="text" placeholder="Search" />
-            </form>
-            <ul className='navbar-right'>
-                <Link to='/' style={{ textDecoration: 'none' }}>
-                    <li>Home</li>
-                </Link>
-                <Link to='/about' style={{ textDecoration: 'none' }}>
-                    <li>About</li>
-                </Link>
-                <Link to='/contact' style={{ textDecoration: 'none' }}>
-                    <li>Contact Us</li>
-                </Link>
-                <Link to='/cart' style={{ textDecoration: 'none' }}>
-                    <li>Your Cart</li>
-                </Link>
-            </ul>
-            
-        </nav>
+    <div
+      style={{
+        backgroundColor: "white",
+        borderBottom: ".5px solid rgb(172, 172, 172)",
+      }}
+    >
+      <nav className="nav-bar navbar-static-top">
+        <Link to="/" style={{ textDecoration: "none" }}>
+          <h1>Bit Buckets</h1>
+        </Link>
+        <form>
+          <IoIosSearch className="searchIcon" size="20px" />
+          <input
+            className="searchBar"
+            type="text"
+            placeholder="Search Products"
+          />
+        </form>
+        <ul className="navbar-right">
+          <Link to="/" style={{ textDecoration: "none" }}>
+            <li>Home</li>
+          </Link>
+          <Link to="/about" style={{ textDecoration: "none" }}>
+            <li>About</li>
+          </Link>
+          <Link to="/contact" style={{ textDecoration: "none" }}>
+            <li>Contact Us</li>
+          </Link>
+          <Link to="/cart" style={{ textDecoration: "none" }}>
+            <li>Your Cart</li>
+          </Link>
+        </ul>
+      </nav>
     </div>
-  )
-}
+  );
+};
 
-export default NavBar
+export default NavBar;
