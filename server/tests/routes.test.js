812990a3d8187fe1299ce44ea1f62570514e1e3c
Nathan Cai <nathan97cai@gmail.com>
2023-03-21 21:49:18 -0400

    added get, post, update and delete endpoint tests. missing get id

--- server/tests/routes.test.js
+++ server/tests/routes.test.js
@@ -1,2 +1,114 @@
-const {Item} = require('../models/index');
+const { Item } = require("../models/index");
+const app = require('../app');
+const request = require("supertest");
+import { sequelize } from "../db";
+import "regenerator-runtime/runtime";
 
+describe("Item model", () => {
+  beforeAll(async () => {
+    await sequelize.sync({ force: true });
+  });
+
+  afterEach(async () => {
+    await Item.destroy({ where: {} });
+  });
+
+  afterAll(async () => {
+    await sequelize.close();
+  });
+
+  test("should return all items", async () => {
+    await Item.create({
+      title: "Test item 1",
+      price: 10,
+      description: "This is test item 1",
+      category: "Test category",
+      image: "test1.jpg",
+    });
+
+    await Item.create({
+      title: "Test item 2",
+      price: 20,
+      description: "This is test item 2",
+      category: "Test category",
+      image: "test2.jpg",
+    });
+
+    const allUsers = await Item.findAll();
+
+    expect(allUsers.length).toBe(2);
+    expect(allUsers[0].title).toBe("Test item 1");
+    expect(allUsers[1].title).toBe("Test item 2");
+  });
+
+  test("should return an empty array if there are no items", async () => {
+    const allUsers = await Item.findAll();
+
+    expect(allUsers.length).toBe(0);
+  });
+
+// need get/:id
+
+  test("should return a 404 error if the item does not exist", async () => {
+    const res = await request(app).get("/999");
+
+    expect(res.statusCode).toBe(404);
+  });
+
+  test("should create an item with all fields", async () => {
+    const item = await Item.create({
+      title: "Test item",
+      price: 10,
+      description: "This is a test item",
+      category: "Test category",
+      image: "test.jpg",
+    });
+
+    expect(item.title).toBe("Test item");
+    expect(item.price).toBe(10);
+    expect(item.description).toBe("This is a test item");
+    expect(item.category).toBe("Test category");
+    expect(item.image).toBe("test.jpg");
+  });
+
+  // it("should not create an item with a missing required field", async () => {
+  //   await expect(
+  //     Item.create({
+  //       price: 10,
+  //       description: "This is a test item",
+  //       category: "Test category",
+  //       image: "test.jpg",
+  //     })
+  //   ).rejects.toThrow(Sequelize.ValidationError);
+  // });
+
+  test("should update an item", async () => {
+    const item = await Item.create({
+      title: "Test item",
+      price: 10,
+      description: "This is a test item",
+      category: "Test category",
+      image: "test.jpg",
+    });
+
+    await item.update({ price: 20 });
+
+    expect(item.price).toBe(20);
+  });
+
+  test("should delete an item", async () => {
+    const item = await Item.create({
+      title: "Test item",
+      price: 10,
+      description: "This is a test item",
+      category: "Test category",
+      image: "test.jpg",
+    });
+
+    await item.destroy();
+
+    const foundItem = await Item.findByPk(item.id);
+
+    expect(foundItem).toBeNull();
+  });
+});
