7e52502513a874458f2aeebad17c4dc7373b99bb
Fatoumata Kaba <fkabanoun@gmail.com>
2023-03-22 13:56:42 -0400

    Added Styling to CSS Product Page

--- public/react/components/ProductPage.js
+++ public/react/components/ProductPage.js
@@ -3,13 +3,13 @@ import './ProductPage.css'
 
 export function ProductPage() {
     return (
-        <div className = "ProductPage"  >
+        <div className = "ProductPage">
             <section classname = "product">
-                <img src="" alt="productimage" />
-                <div className = "product description">
-                    <h1 className = "product Title">Product Title</h1>
-                    <h2 className = "product Price">$XX.XX</h2>
-                    <div className ="productSpecs" >
+                <img src="" alt="productImage"/>
+                <div className = "productDescription">
+                    <h1 className = "productTitle">Product Title</h1>
+                    <h2 className = "productPrice">$XX.XX</h2>
+                    <div className ="productSpecs">
                         <p> Product Description with specs and details</p>
                     </div>
                     <div className = "CartButtons">
