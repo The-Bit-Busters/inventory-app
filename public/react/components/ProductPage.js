bdf2017f2e1a7a3533ec32350c7c02596b2d65a9
Sonam Tsering <sonam.tsering@sonam.tsering-MacBook-Pro>
2023-03-22 16:54:18 -0400

    made some changes due to merge conflicts

--- public/react/components/ProductPage.js
+++ public/react/components/ProductPage.js
@@ -30,27 +30,4 @@ export function ProductPage({items}) {
                 </div>
         )}
     }
-    // return (
-    //     <div className = "ProductPage"  >
-    //         <section className = "product">
-    //             <img src="" alt="productimage" />
-    //             <div className = "product description">
-    //                 <h1 className = "product Title">Product Title</h1>
-    //                 <h2 className = "product Price">$XX.XX</h2>
-    //                 <div className ="productSpecs" >
-    //                     <p> Product Description with specs and details</p>
-    //                 </div>
-    //                 <div className = "CartButtons">
-    //                     <button className = "buttons">Add to Cart</button>
-    //                     <button className = "buttons">View Cart</button>
-    //                 </div>
-    //             </div>
-    //         </section>
-    //         <section className = "Update button">
-    //             <button className = "button">
-    //                 Update Item
-    //             </button>
-    //         </section>
-    //     </div>
-    // )
 }
