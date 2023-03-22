d1c17b22fcd524821e3af52fdfbb801f35678a69
Sonam Tsering <sonam.tsering@sonam.tsering-MacBook-Pro>
2023-03-21 23:27:16 -0400

    basic jsx and styling for about page complete

--- public/react/components/About.js
+++ public/react/components/About.js
@@ -4,11 +4,11 @@ import '../stylesheets/about.css'
 const About = () => {
   return (
     <div className="wrapper">
-        <div className="box a">A</div>
-        <div className="box b">B</div>
-        <div className="box c">C</div>
-        <div className="box d">D</div>
-        <div className="box e">E</div>
+        <div className="box a">Mission Statement:  Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui, ut sed? Aperiam nisi repudiandae quas aspernatur minus labore quia quae facilis, doloribus, recusandae, at velit similique explicabo amet nostrum veritatis!</div>
+        <div className="box b">Core Values: Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui, ut sed? Aperiam nisi repudiandae quas aspernatur minus labore quia quae facilis, doloribus, recusandae, at velit similique explicabo amet nostrum veritatis!</div>
+        <div className="box c">Company History: Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui, ut sed? Aperiam nisi repudiandae quas aspernatur minus labore </div>
+        <div className="box d">Target Market: Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui, ut sed? Aperiam nisi repudiandae quas aspernatur minus labore quia quae facilis, doloribus, recusandae, at velit similique explicabo amet nostrum veritatis!</div>
+        <div className="box e">Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui, ut sed? Aperiam nisi repudiandae quas aspernatur minus labore quia quae facilis, doloribus, recusandae, at velit similique explicabo amet nostrum veritatis!</div>
     </div>
   )
 }
