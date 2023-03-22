19bcde672277bef6dff8d4dcd58da10789323b96
Sonam Tsering <sonam.tsering@sonam.tsering-MacBook-Pro>
2023-03-22 11:43:51 -0400

    contact styling and structure

--- public/react/components/Contact.js
+++ public/react/components/Contact.js
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from "react";
+import '../stylesheets/contact.css';
 
-const Contact = () => {
-  return (
-    <div>Contact</div>
-  )
-}
+const Form = () => {
+  return <div className='contactForm'>
+    <form className="mainForm">  
+        <label>YOUR NAME</label>
+        <input className="contactInputs" type="text" name="name" placeholder='Name'></input>
+        <label>EMAIL</label>
+        <input className="contactInputs" type="text" name="name" placeholder='Email'></input>
+        <label>MESSAGE</label>
+        <textarea rows="6" placeholder='Type your message here'></textarea>
+        <button className='btn' type="submit">SEND</button>
+    </form>
+  </div>;
+};
 
-export default Contact
+export default Form;
