d5cdceb91db6cfa806f26f693fa46e89bbef281f
Nathan Cai <nathan97cai@gmail.com>
2023-03-23 15:25:05 -0400

    added changes, needs styling

--- public/react/components/NavBar.js
+++ public/react/components/NavBar.js
@@ -3,7 +3,7 @@ import "../stylesheets/navbar.css";
 import { Link } from "react-router-dom";
 import { IoIosSearch } from "react-icons/io";
 import { FaBars, FaTimes } from 'react-icons/fa';
-import logo from "../stylesheets/logo.png"
+import logo from "../stylesheets/logo1.png"
 
 
 const NavBar = () => {
@@ -24,7 +24,7 @@ const NavBar = () => {
     >
       <nav className="nav-bar navbar-static-top">
         <Link to="/" style={{ textDecoration: "none" }}>
-        <img className="logo-img" src={logo} alt="logo"/>
+        <img src={logo} alt="logo"/>
         </Link>
         <form>
           <IoIosSearch className="searchIcon" size="20px" />
