6d0beaffd32f5ddda9a23c625eb4b85c553d8821
Sonam Tsering <sonam.tsering@sonam.tsering-MacBook-Pro>
2023-03-23 12:57:35 -0400

    cart price functionality working

--- public/react/components/Cart.js
+++ public/react/components/Cart.js
@@ -3,16 +3,36 @@ import { Container, Table, Row, Button } from "react-bootstrap";
 import "../stylesheets/cart.css";
 
 const Cart = ({ cart }) => {
+  const [subTotal, setSubTotal] = React.useState(0);
+  const [tax, setTax] = React.useState(0);
   const [total, setTotal] = React.useState(0);
+
+  React.useEffect(() => {
+    let total = 0;
+    cart.forEach((item) => {
+      total += item.price;
+      total+= item.price * 0.13;
+    });
+    setTotal(total);
+  }, [cart]);
+
 
   React.useEffect(() => {
     let sum = 0;
     cart.forEach((item) => {
       sum += item.price;
     });
-    setTotal(sum);
+    setSubTotal(sum);
   }, [cart]);
 
+  React.useEffect(() => {
+    let tax = 0;
+    cart.forEach((item) => {
+      tax += item.price * 0.13;
+    });
+    setTax(tax);
+  }, [cart]);
+  
 
   return (
     <div>
@@ -45,12 +65,12 @@ const Cart = ({ cart }) => {
           <div className="col-3 cartSum boxShadaw bg-light p-4">
             <h5 className="text-left mb-4 pb-2">Cart Price</h5>
             <div className="d-flex justify-content-between mb-3">
-              <h6 className="fw-normal">Tax :</h6>
-              <span>$123123</span>
+              <h6 className="fw-normal">SubTotal Price :</h6>
+              <span>${subTotal}</span>
             </div>
             <div className="d-flex justify-content-between mb-4">
-              <h6 className="fw-normal">SubTotal Price :</h6>
-              <span>$2002</span>
+              <h6 className="fw-normal">Tax :</h6>
+              <span>${tax}</span>
             </div>
             <div className="d-flex justify-content-between fw-bold">
               <h6>Total Price :</h6>
