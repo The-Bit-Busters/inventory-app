01ba4732611ccd170e0eedfb0b59505aad13959b
Nathan Cai <nathan97cai@gmail.com>
2023-03-23 14:39:14 -0400

    added images and changed footer

--- public/react/components/Footer.js
+++ public/react/components/Footer.js
@@ -32,27 +32,21 @@ const Footer = () => {
           <div className="email">
             <p>
               <FaMailBulk size={20} style={{ marginRight: "2rem" }} />
-              BitBucketW@gmail.com{" "}
+              BitBucket@gmail.com
             </p>
           </div>
         </div>
         <div className="right">
-          <h4 className="about">Lorem Ipsum</h4>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui, ut
-            sed? Aperiam nisi repudiandae quas aspernatur minus labore quia quae
-            facilis, doloribus, recusandae, at velit similique explicabo amet
-            nostrum veritatis!
-          </p>
+          <h4 className="about">Follow Us On Social Media!</h4>
           <div className="social">
             <a href="https://www.linkedin.com/" target="_blank">
-              <FaLinkedin size={20} style={{ marginRight: "1rem" }} />
+              <FaLinkedin size={25} style={{ marginRight: "1rem" }} />
             </a>
             <a href="https://www.instagram.com/" target="_blank">
-              <FaInstagram size={20} style={{ marginRight: "1rem" }} />
+              <FaInstagram size={25} style={{ marginRight: "1rem" }} />
             </a>
             <a href="https://www.facebook.com/" target="_blank">
-              <FaFacebook size={20} style={{ marginRight: "1rem" }} />
+              <FaFacebook size={25} style={{ marginRight: "1rem" }} />
             </a>
           </div>
         </div>
